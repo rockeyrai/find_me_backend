d02b2a705e45bf023da31d96c3dcb4970a868f04
rockeyrai <rockeyrai234@gmail.com>
2026-03-14 22:21:47 +0545

    connecting the database

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database error");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
